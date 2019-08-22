a86805fca1cd49fb3e652d4cec4c08701af771c9
Bruno <brunosuportefs@gmail.com>
2019-08-22 10:01:50 -0300

    Configurando banco de dados com Jpa e Postgresql

--- src/main/java/br/com/bruno/javaavancado/JavaAvancadoApplication.java
+++ src/main/java/br/com/bruno/javaavancado/JavaAvancadoApplication.java
@@ -2,8 +2,10 @@ package br.com.bruno.javaavancado;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages="br.com.bruno.javaavancado.model")
 public class JavaAvancadoApplication {
 
 	public static void main(String[] args) {
