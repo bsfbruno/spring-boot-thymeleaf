a3ddc92fd6560bef677a353f89761111f188e1da
Bruno <brunosuportefs@gmail.com>
2019-08-24 09:42:08 -0300

    Editando cadastro com Thymeleaf

--- src/main/java/br/com/bruno/javaavancado/controller/PessoaController.java
+++ src/main/java/br/com/bruno/javaavancado/controller/PessoaController.java
@@ -1,9 +1,12 @@
 package br.com.bruno.javaavancado.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,8 +21,10 @@ public class PessoaController {
 	private PessoaRepository pessoaRepository;
 
 	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
-	public String inicio() {
-		return "cadastro/cadastropessoa";
+	public ModelAndView inicio() {
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		modelAndView.addObject("pessoaObj", new Pessoa());
+		return modelAndView;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/salvarpessoa")
@@ -33,6 +38,15 @@ public class PessoaController {
 		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
 		List<Pessoa> pessoasList = pessoaRepository.findAll();
 		modelAndView.addObject("pessoas", pessoasList);
+		modelAndView.addObject("pessoaObj", new Pessoa());
+		return modelAndView;
+	}
+	
+	@GetMapping("/editarpessoa/{idpessoa}")
+	public ModelAndView editar(@PathVariable("idpessoa") Long idpessoa) {
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
+		modelAndView.addObject("pessoaObj", pessoa);
 		return modelAndView;
 	}
 }
